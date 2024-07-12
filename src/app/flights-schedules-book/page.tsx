7ed5af996e55ad22719e96d74eb3778a2492cb08
Abhinn Sarkar <abhinnsarkar@gmail.com>
2024-07-11 18:09:27 -0700

    set up the push page for searching for available flights

--- src/app/flights-schedules-book/page.tsx
+++ src/app/flights-schedules-book/page.tsx
@@ -1,7 +1,293 @@
-import React from "react";
+"use client";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+import TextField from "@mui/material/TextField";
+import FormHelperText from "@mui/material/FormHelperText";
+import { Button } from "@mui/material";
 
-const page = () => {
-    return <div>Book Flights</div>;
+interface SeatBookingAllocationInfo {
+    seatAllocationId: string;
+    flightScheduleId: string;
+    modelSeatId: string;
+    available: boolean;
+    seatNumber: string;
+    seatClass: string;
+    flightModelNameKey: string;
+}
+
+interface FlightScheduleDTO {
+    flightScheduleId: string;
+    flightId: string;
+    departureDate: string;
+    flightNumber: string;
+    flightModelNameKey: string;
+    routeId: string;
+    originAirportCode: string;
+    departureLocation: string;
+    destinationAirportCode: string;
+    destinationLocation: string;
+    seats: SeatBookingAllocationInfo[];
+}
+
+interface ApiResponse {
+    data: FlightScheduleDTO[];
+    messages: string[];
+    returnCode: number;
+}
+
+const Page = () => {
+    const currYear = new Date().getFullYear();
+    const currentMonth = new Date().getMonth() + 1; // Months are zero-indexed
+    const currentDay = new Date().getDate();
+
+    const [month, setMonth] = useState(currentMonth.toString());
+    const [availableMonths, setAvailableMonths] = useState<number[]>([]);
+    const monthsDictionary: { [key: number]: string } = {
+        1: "January",
+        2: "February",
+        3: "March",
+        4: "April",
+        5: "May",
+        6: "June",
+        7: "July",
+        8: "August",
+        9: "September",
+        10: "October",
+        11: "November",
+        12: "Decemeber",
+    };
+
+    const [day, setDay] = useState(currentDay.toString());
+    const [year, setYear] = useState(currYear.toString());
+    const [currentYear, setCurrentYear] = useState(currYear.toString());
+    const [years, setYears] = useState<number[]>([]);
+    const [daysInMonth, setDaysInMonth] = useState<number[]>([]);
+    const [error, setError] = useState("");
+
+    useEffect(() => {
+        const futureYears: number[] = [];
+        for (let i = 0; i <= 5; i++) {
+            futureYears.push(parseInt(currentYear) + i);
+        }
+        setYears(futureYears);
+    }, [currentYear]);
+
+    const handleYearChange = (event: SelectChangeEvent) => {
+        setYear(event.target.value as string);
+
+        const getDaysInMonth = (year: number, month: number) => {
+            return new Date(year, month, 0).getDate();
+        };
+
+        setAvailableMonths(
+            Array.from(
+                { length: Object.keys(monthsDictionary).length },
+                (_, i) => i + 1
+            )
+        );
+
+        const days = getDaysInMonth(parseInt(year), parseInt(month));
+        setDaysInMonth(Array.from({ length: days }, (_, i) => i + 1));
+
+        setError("");
+    };
+
+    const handleMonthChange = (event: SelectChangeEvent) => {
+        setMonth(event.target.value as string);
+        setError("");
+    };
+
+    const handleDayChange = (event: SelectChangeEvent) => {
+        setDay(event.target.value as string);
+        setError("");
+    };
+
+    useEffect(() => {
+        if (year && month) {
+            const getDaysInMonth = (year: number, month: number) => {
+                return new Date(year, month, 0).getDate();
+            };
+
+            const days = getDaysInMonth(parseInt(year), parseInt(month));
+
+            const tempDaysInMonth = Array.from(
+                { length: days },
+                (_, i) => i + 1
+            );
+
+            if (year === currentYear) {
+                const lastAvailableMonth = 12;
+
+                const monthsFromCurrentMonth = Array.from(
+                    { length: lastAvailableMonth - (currentMonth - 1) }, // Adjusting for 1-based index
+                    (_, index) => currentMonth + index
+                );
+
+                setAvailableMonths(monthsFromCurrentMonth);
+            }
+
+            if (year === currentYear && parseInt(month) === currentMonth) {
+                const daysFromCurrentDay = Array.from(
+                    { length: tempDaysInMonth.length - (currentDay - 1) }, // Adjusting for 1-based index
+                    (_, index) => currentDay + index
+                );
+
+                setDaysInMonth(daysFromCurrentDay);
+            } else {
+                setDaysInMonth(Array.from({ length: days }, (_, i) => i + 1));
+            }
+
+            // Validate the selected day
+            if (parseInt(day) > days) {
+                setError("Invalid day for the selected month and year.");
+                setDay("");
+            } else {
+                setError("");
+            }
+        }
+    }, [year, month, day, currentDay, currentMonth, currentYear]);
+
+    const [leaveFrom, setLeaveFrom] = useState("");
+    const [destination, setDestination] = useState("");
+
+    const handleLeaveFromChange = (
+        event: React.ChangeEvent<HTMLInputElement>
+    ) => {
+        setLeaveFrom(event.target.value);
+    };
+
+    const handleDestinationChange = (
+        event: React.ChangeEvent<HTMLInputElement>
+    ) => {
+        setDestination(event.target.value);
+    };
+
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        // Construct the request body
+        const requestData = {
+            departureLocation: leaveFrom,
+            destinationLocation: destination,
+            departureMonth: month,
+            departureDay: day,
+            departureYear: year,
+        };
+
+        console.log(requestData);
+
+        axios
+            .get<ApiResponse>("http://localhost:8080/flight-schedule", {
+                params: requestData,
+            })
+            .then((response) => {
+                console.log(response); // Log or process the response data
+                console.log(response.data); // Log or process the response data
+                console.log(response.data.data); // Log or process the response data
+            })
+            .catch((error) => {
+                console.error("Error fetching flights schedules:", error); // Handle errors
+            });
+    };
+
+    return (
+        <Box>
+            <form onSubmit={handleSubmit}>
+                <Box>
+                    <FormControl sx={{ width: "10%", ml: "2%" }}>
+                        <InputLabel id="year-select-label">Year</InputLabel>
+                        <Select
+                            labelId="year-select-label"
+                            id="year-select"
+                            value={year}
+                            label="Year"
+                            onChange={handleYearChange}
+                        >
+                            {years.map((year) => (
+                                <MenuItem key={year} value={year}>
+                                    {year}
+                                </MenuItem>
+                            ))}
+                        </Select>
+                    </FormControl>
+
+                    <FormControl sx={{ width: "10%", ml: "2%" }}>
+                        <InputLabel id="month-select-label">Month</InputLabel>
+                        <Select
+                            labelId="month-select-label"
+                            id="month-select"
+                            value={month}
+                            label="Month"
+                            onChange={handleMonthChange}
+                        >
+                            {availableMonths.map((month, index) => (
+                                <MenuItem key={month} value={month}>
+                                    {monthsDictionary[month]}
+                                </MenuItem>
+                            ))}
+                        </Select>
+                    </FormControl>
+
+                    <FormControl
+                        sx={{ width: "10%", ml: "2%" }}
+                        error={Boolean(error)}
+                    >
+                        <InputLabel id="day-select-label">Day</InputLabel>
+                        <Select
+                            labelId="day-select-label"
+                            id="day-select"
+                            value={day}
+                            label="Day"
+                            onChange={handleDayChange}
+                        >
+                            {daysInMonth.map((day) => (
+                                <MenuItem key={day} value={day}>
+                                    {day}
+                                </MenuItem>
+                            ))}
+                        </Select>
+                        {error && <FormHelperText>{error}</FormHelperText>}
+                    </FormControl>
+                </Box>
+
+                <Box sx={{ mt: "2%" }}>
+                    <TextField
+                        required
+                        id="leave-from-input"
+                        label="Leave From"
+                        variant="outlined"
+                        sx={{ width: "10%", ml: "2%" }}
+                        value={leaveFrom}
+                        onChange={handleLeaveFromChange}
+                    />
+                    <TextField
+                        required
+                        id="destination-input"
+                        label="Destination"
+                        variant="outlined"
+                        sx={{ width: "10%", ml: "2%" }}
+                        value={destination}
+                        onChange={handleDestinationChange}
+                    />
+                </Box>
+
+                <Box sx={{ mt: "2%" }}>
+                    <Button
+                        type="submit"
+                        variant="contained"
+                        sx={{ width: "10%", ml: "2%" }}
+                    >
+                        Search
+                    </Button>
+                </Box>
+            </form>
+        </Box>
+    );
 };
 
-export default page;
+export default Page;
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,3 +1,68 @@
+// "use client";
+// import React from "react";
+// import { usePathname } from "next/navigation";
+
+// import HomeIcon from "@mui/icons-material/Home";
+// import Person2Icon from "@mui/icons-material/Person2";
+// import AirplanemodeActiveIcon from "@mui/icons-material/AirplanemodeActive";
+// import AirplaneTicketIcon from "@mui/icons-material/AirplaneTicket";
+// import { pink } from "@mui/material/colors";
+
+// export default function Header() {
+//     const pages = [
+//         { name: `Home`, url: "/", icon: <HomeIcon /> },
+//         { name: "Profile", url: "/profile", icon: <Person2Icon /> },
+//         { name: "Flights", url: "/flights", icon: <AirplanemodeActiveIcon /> },
+//         {
+//             name: "Book a Flight",
+//             url: "/flights-schedules-book",
+//             icon: <AirplaneTicketIcon />,
+//         },
+//     ];
+
+//     const path = usePathname();
+//     console.log(path);
+
+//     return (
+//         <header
+//             className="bg-red-800 w-full font-serif"
+//             style={{ height: "10vh" }}
+//         >
+//             <nav className="flex justify-between items-center h-full w-full">
+//                 <div className="w-1/3">
+//                     <h1
+//                         style={{ marginLeft: "5%" }}
+//                         className="text-3xl text-zinc-50"
+//                     >
+//                         ASARK Airlines
+//                     </h1>
+//                 </div>
+//                 <ul
+//                     className="flex items-center h-full"
+//                     style={{ width: "34%" }}
+//                 >
+//                     {pages.map((page) => (
+//                         <li
+//                             style={{ height: "50%" }}
+//                             className={` flex items-center justify-center px-4 cursor-pointer transition-transform duration-300 ${
+//                                 path === page.url
+//                                     ? "text-zinc-400"
+//                                     : "text-zinc-50"
+//                             }  hover:text-lg`}
+//                             key={page.url}
+//                         >
+//                             <a className="flex items-center" href={page.url}>
+//                                 {page.name}
+//                                 {page.icon}
+//                             </a>
+//                         </li>
+//                     ))}
+//                 </ul>
+//             </nav>
+//         </header>
+//     );
+// }
+
 "use client";
 import React from "react";
 import { usePathname } from "next/navigation";
@@ -7,6 +72,20 @@ import Person2Icon from "@mui/icons-material/Person2";
 import AirplanemodeActiveIcon from "@mui/icons-material/AirplanemodeActive";
 import AirplaneTicketIcon from "@mui/icons-material/AirplaneTicket";
 import { pink } from "@mui/material/colors";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Divider from "@mui/material/Divider";
+import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import MenuIcon from "@mui/icons-material/Menu";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
 
 export default function Header() {
     const pages = [
@@ -24,41 +103,52 @@ export default function Header() {
     console.log(path);
 
     return (
-        <header
-            className="bg-red-800 w-full font-serif"
-            style={{ height: "10vh" }}
-        >
-            <nav className="flex justify-between items-center h-full w-full">
-                <div className="w-1/3">
-                    <h1
-                        style={{ marginLeft: "5%" }}
-                        className="text-3xl text-zinc-50"
+        <Box sx={{ display: "flex" }}>
+            <CssBaseline />
+            <AppBar component="nav">
+                <Toolbar>
+                    <IconButton
+                        color="inherit"
+                        aria-label="open drawer"
+                        edge="start"
+                        // onClick={handleDrawerToggle}
+                        sx={{ mr: 2, display: { sm: "none" } }}
                     >
-                        ASARK Airlines
-                    </h1>
-                </div>
-                <ul
-                    className="flex items-center h-full"
-                    style={{ width: "34%" }}
-                >
-                    {pages.map((page) => (
-                        <li
-                            style={{ height: "50%" }}
-                            className={` flex items-center justify-center px-4 cursor-pointer transition-transform duration-300 ${
-                                path === page.url
-                                    ? "text-zinc-400"
-                                    : "text-zinc-50"
-                            }  hover:text-lg`}
-                            key={page.url}
-                        >
-                            <a className="flex items-center" href={page.url}>
-                                {page.name}
-                                {page.icon}
-                            </a>
-                        </li>
-                    ))}
-                </ul>
-            </nav>
-        </header>
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography
+                        variant="h6"
+                        component="div"
+                        sx={{
+                            flexGrow: 1,
+                            display: { xs: "none", sm: "block" },
+                        }}
+                    >
+                        MUI
+                    </Typography>
+                    <Box sx={{ display: { xs: "none", sm: "block" } }}>
+                        {pages.map((page) => (
+                            <Button key={page.url} sx={{ color: "#fff" }}>
+                                <a
+                                    href={page.url}
+                                    className={`flex items-center justify-center px-4 cursor-pointer transition-transform duration-300 ${
+                                        path === page.url
+                                            ? "text-zinc-400"
+                                            : "text-zinc-50"
+                                    }  hover:text-zinc-600`}
+                                >
+                                    {page.name}
+                                    {page.icon}
+                                </a>
+                            </Button>
+                        ))}
+                    </Box>
+                </Toolbar>
+            </AppBar>
+            <nav></nav>
+            <Box component="main" sx={{ p: 3 }}>
+                <Toolbar />
+            </Box>
+        </Box>
     );
 }
